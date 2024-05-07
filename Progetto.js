5417dc8ced50f29c6b790bdc76d68330ee4e8b5f
CristianBocale <cristianbocale.cb@gmail.com>
2024-05-07 18:50:07 +0200

    Create Progetto.js

---
+++ Progetto.js
@@ -0,0 +1,27 @@
+/*Inizializzazione progetto
+
+Sviluppare un'applicazione JavaScript che consenta di gestire una lista della spesa consentendo almeno le seguenti operazioni:
+
+Versione 1 - Base
+- Aggiunta e rimozione di elementi dalla lista della spesa.
+- Visualizzazione della lista.
+
+Versione 2 - Avanzata
+- Filtraggio e ordinamento degli elementi.
+- Ricerca di un elemento nella lista della spesa.
+
+
+Idea struttura iniziale del progetto che può variare in futuro:
+
+un main con un menu dove è possibile decidere cosa fare come ad esempio decidere di vedere la lista,
+aggiungere elementi ad essa, eliminarli ecc.
+
+- L'aggiunta deve essere una funzione chiamata Aggiungi().
+- La rimozione deve essere una funzione chiamata Rimuovi().
+- Deve esistere una funzione che ordini gli elementi della lista in ordine alfabetico detta Ordina().
+- Deve esistere una funzione che permetta la ricerca di un elemento all'interno della lista che deve
+gestire caratteri maiuscoli e minuscoli indistintamente per permettere una ricerca facile e intuitiva.
+
+- Inizieremo con una versione semplice che fa uso di vettori e strutture come oggetti ecc.
+- In seguito aggiorneremo mettendo i file.
+*/
