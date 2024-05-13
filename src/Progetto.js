e58510171a6bfb0c51b69e38c5d3a7295d1cab21
CristianBocale <cristianbocale.cb@gmail.com>
2024-05-13 22:36:55 +0200

    Primi Pezzi del progetto
    Aggiunta del Main e del menu e creazione della Classe ListaDellaSpesa per la gestione di tutto quello che riguarderà la lista. Unico metodo presente: Aggiungi().

--- src/Progetto.js
+++ src/Progetto.js
@@ -1,11 +1,67 @@
 const prompt=require("prompt-sync")();
+//Classe Lista che contiene un elemento Lista di tipo MAP e vari metodi tra cui Aggiungi.
+class ListaDellaSpesa{
+    Lista=new Map();
 
-let listaSpesa = [];
-
-function aggiungiAlimento()
-{
-    let alimento=prompt("Inserire un nuovo alimento:");
-    listaSpesa.push(alimento);
+    /*
+    Con il metodo aggiungi è possibile sia aggiungere nuovi elementi appartenenti a categorie
+    non ancora presenti, sia aggiungere elementi a categorie già create. 
+    */
+    Aggiungi(prodotto,quantita,categoria){        
+        if(!this.Lista.has(categoria)){
+            this.Lista.set(categoria,new Map());
+        }
+        this.Lista.get(categoria).set(prodotto,quantita);
+    }
 }
 
-aggiungiAlimento();
+/*
+Il main presenta un menu dove è possibile scegliere le azioni da eseguire. sono inizializzati tutti i case
+seppure manchino ancora alcune delle funzioni.
+*/
+function main(){
+    const Lista=new ListaDellaSpesa();
+    let scelta;
+    do{
+        console.log("Scegliere cosa fare (1-5)");
+        console.log("1-Aggiungere elemento alla lista della spesa");
+        console.log("2-Visualizzare la lista della spesa");
+        console.log("3-Rimuovere un elemento dalla lista della spesa");
+        console.log("4-Ricerca nella lista della spesa");
+        console.log("5-Esci");
+        scelta=parseInt(prompt(">> "));
+        switch(scelta){
+            case 1:{
+                let prodotto=prompt("Inserisci prodotto: ").toLowerCase();
+                let quantita=prompt("Inserisci quantità da acquistare: ");
+                let categoria=prompt("Inserisci categoria prodotto (Es cibo,bevande,ecc): ").toUpperCase();
+                Lista.Aggiungi(prodotto,quantita,categoria);
+                break;
+            }
+            case 2:{
+                Lista.Stampa();
+                break;
+            }
+            case 3:{
+                let prodotto=prompt("Inserisci prodotto o categoria da eliminare dalla lista: ");
+                Lista.Elimina(prodotto);
+                break;
+            }
+            case 4:{
+                let prodotto=prompt("Inserisci prodotto o categoria di prodotti da cercare nella lista: ");
+                Lista.Cerca(prodotto);
+                break;
+            }
+            case 5:{
+                break;
+            }
+            default:{
+                console.log("Opzione non valida");
+                break;
+            }
+        }
+    }while(scelta!==5);
+
+}
+
+main();
